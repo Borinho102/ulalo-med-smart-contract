285ba20207a3a9655d02e4344c7c5f80aa02e1ca
Borinho102 <borissandeu0@gmail.com>
2025-01-15 13:14:15 +0100

    APi-v2

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ app.post('/store', uploader.single('file'), async (req, res) => {
         const { userAddress, fileType } = req.body;
         const file = req.file;
 
-        console.log(req.body);
+        console.log(req.body, file);
 
         const key = crypto.createHash('sha256').update(userAddress, 'utf8').digest().slice(0, 32);
         const iv = Buffer.alloc(16, 0);
